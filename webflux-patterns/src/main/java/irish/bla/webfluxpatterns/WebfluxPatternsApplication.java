7b4fa56dcd9047bfa55dd6a826174d353325a0cd
weoamathior <weoamathior@gmail.com>
2024-01-30 07:09:05 -0600

    initial commit

---
+++ webflux-patterns/src/main/java/irish/bla/webfluxpatterns/WebfluxPatternsApplication.java
@@ -0,0 +1,13 @@
+package irish.bla.webfluxpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxPatternsApplication.class, args);
+	}
+
+}
