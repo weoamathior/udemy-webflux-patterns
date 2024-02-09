5bdedc19be3d1e2adca9121346c046dfbb026f16
weoamathior <weoamathior@gmail.com>
2024-02-09 07:28:06 -0600

    client side rate limiter example

--- webflux-patterns/src/main/java/irish/bla/webfluxpatterns/sec09/ratelimiter/client/ReviewClient.java
+++ webflux-patterns/src/main/java/irish/bla/webfluxpatterns/sec09/ratelimiter/client/ReviewClient.java
@@ -1,16 +1,17 @@
 package irish.bla.webfluxpatterns.sec09.ratelimiter.client;
 
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import irish.bla.webfluxpatterns.sec09.ratelimiter.dto.Review;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
-import java.time.Duration;
 import java.util.Collections;
 import java.util.List;
-
+@Slf4j
 @Service
 public class ReviewClient {
     private final WebClient client;
@@ -21,6 +22,7 @@ public class ReviewClient {
                 .build();
     }
 
+    @RateLimiter(name = "review-service", fallbackMethod = "fallback")
     public Mono<List<Review>> getReview(Integer id) {
         return client
                 .get()
@@ -28,12 +30,12 @@ public class ReviewClient {
                 .retrieve()
                 .onStatus(HttpStatusCode::is4xxClientError, response -> Mono.empty())
                 .bodyToFlux(Review.class)
-                .collectList()
-                .retry(5)
-//                .retryWhen(Retry.fixedDelay(10, Duration.ofMillis(500)))
-//                .retryWhen(Retry.backoff(10, Duration.ofMillis(500)))
-                .timeout(Duration.ofMillis(300))
-                .onErrorReturn(Collections.emptyList());
+                .collectList();
 
+    }
+
+    public Mono<List<Review>> fallback(Integer id, Throwable t) {
+        log.info("too many calls == $$$! " + t.getMessage());
+        return Mono.just(Collections.emptyList());
     }
 }
