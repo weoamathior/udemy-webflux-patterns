4abecf96c78f3f8d590146fa644ab341a3539c91
weoamathior <weoamathior@gmail.com>
2024-02-09 13:08:48 -0600

    bulkhead demo

--- webflux-patterns/src/main/java/irish/bla/webfluxpatterns/sec10/bulkhead/controller/FibController.java
+++ webflux-patterns/src/main/java/irish/bla/webfluxpatterns/sec10/bulkhead/controller/FibController.java
@@ -6,10 +6,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
 
 @RestController
 @RequestMapping("sec10")
 public class FibController {
+    // Define max number of parallel calls made at a time
+    private final Scheduler scheduler = Schedulers.newParallel("fib", 6);
     /*
     Pretend this is a cpu-intensive task
     goal: 5 requests / 20 seconds
@@ -17,6 +21,7 @@ public class FibController {
     @GetMapping("fib/{input}")
     public Mono<ResponseEntity<Long>> doubleInput(@PathVariable Long input) {
         return Mono.fromSupplier(() -> findFib(input))
+                .subscribeOn(scheduler)
                 .map(ResponseEntity::ok);
 
     }
